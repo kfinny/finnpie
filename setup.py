a5d1b29d07e48cf5ab5b39d65cc4a322b028589c
Kevin <kfinnigin@cylance.com>
2018-06-22 15:39:27 -0400

    Refactored to support pkgutil namespacing

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='finnpie',
+setup(name='kfinny.finnpie',
       version='1',
       description='A simple library for packaging useful RE functions',
       url='https://github.com/kfinny/finnpie',
       author='Kevin Finnigin',
       author_email='kevin@finnigin.net',
       license='MIT',
-      packages=['finnpie'],
+      packages=find_packages(),
       zip_safe=False)
